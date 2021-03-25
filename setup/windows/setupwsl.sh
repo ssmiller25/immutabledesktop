042d70ab47e293b4bbfe0fbf0665f73c6cc39101
Steve Miller <me@r15cookie.com>
2021-03-25 06:47:52 -0400

    Windows install rearrage - some docker build stuff

---
+++ setup/windows/setupwsl.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+echo "Minimal WSL installation setup"
+echo "Software Install"
+sudo apt install -y build-essential jq 
+
