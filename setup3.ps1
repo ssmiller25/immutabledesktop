318ace120faaa6d3951ea5e15af6b6bc4b04425a
Steve Miller <me@r15cookie.com>
2021-03-24 22:46:30 -0400

    Update setup3.ps1

--- setup3.ps1
+++ setup3.ps1
@@ -2,3 +2,4 @@ echo "Chocolate Installs"
 
 choco install -y microsoft-windows-terminal vcxsrv docker-desktop wsl-ubuntu-2004
 echo "Go to https://docs.microsoft.com/en-us/windows/wsl/install-win10#step-4---download-the-linux-kernel-update-package to complete wsl2 kernel installation"
+echo "Reboot, then manually start `wsl.exe` and go through initial setup"
