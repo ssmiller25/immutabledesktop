a95a98a377d2650c7bc2a016b16b75e2df8d49c9
Steve Miller <me@r15cookie.com>
2021-03-25 06:59:27 -0400

    ssh key stuff

--- setup/windows/setupwsl.sh
+++ setup/windows/setupwsl.sh
@@ -10,11 +10,20 @@ cleanup() {
   # script cleanup here
 }
 
+echo "Update base distr"
+sudo apt-get update
+sudo apt-get -y dist-upgrade
+
 echo "Minimal WSL installation setup"
 echo "Software Install"
-sudo apt install -y build-essential jq 
+sudo apt install -y build-essential jq  
 
 echo "Git Configuration"
 git config --global user.name "Steve Miller"
 git config --global user.email "me@r15cookie.com"
 git config --global core.autocrlf input
+
+echo "ssh-key"
+sudo apt-get install -y keychain
+echo "/usr/bin/keychain --nogui $HOME/.ssh/id_rsa" >> ${HOME}/.bashrc
+echo "source $HOME/.keychain/$HOSTNAME-sh" >> ${HOME}/.bashrc
