faf67fb44eae4bba771eb8f4af590021a787ac01
Steve Miller <me@r15cookie.com>
2021-03-25 05:38:07 -0400

    Update setup3.ps1

--- setup3.ps1
+++ setup3.ps1
@@ -4,3 +4,5 @@ choco install -y microsoft-windows-terminal vcxsrv docker-desktop wsl-ubuntu-200
 echo "Go to https://docs.microsoft.com/en-us/windows/wsl/install-win10#step-4---download-the-linux-kernel-update-package to complete wsl2 kernel installation"
 echo "Reboot, then manually start `wsl.exe` and go through initial setup"
 echo "Log into docker hub in docker-desktop"
+echo "Run ssh-keygen from windows"
+echo "Update Windows Terminal to make WSL primary shell"
