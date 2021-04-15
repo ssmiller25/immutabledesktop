4a6fb8843ab15acdd9365ac9ead9200e3227da00
Steve Miller <me@r15cookie.com>
2021-04-15 18:50:30 -0400

    Updates to streamline to JUST minimum installs

--- setup/windows/setup3.ps1
+++ setup/windows/setup3.ps1
@@ -1,22 +1,17 @@
-echo "Enable Windows SSH Client and Agent"
-Add-WindowsCapability -Online -Name OpenSSH.Client
-Get-Service -Name ssh-agent | Set-Service -StartupType Automatic
-Start-Service ssh-agent
-
 echo "Chocolate Installs"
 
-choco install -y microsoft-windows-terminal vcxsrv docker-desktop wsl-ubuntu-2004 vscode git etcher
+choco install -y microsoft-windows-terminal vcxsrv docker-desktop wsl-ubuntu-2004 vscode
 echo "Setting Ubuntu 20.04 as default Linux distro"
 wsl --set-default Ubuntu-20.04
-echo "Go to https://docs.microsoft.com/en-us/windows/wsl/install-win10#step-4---download-the-linux-kernel-update-package to complete wsl2 kernel installation"
 echo "Reboot, then manually start `wsl.exe` and go through initial setup"
+Read-Host -Prompt "Press Enter to continue"
 echo "Log into docker hub in docker-desktop"
-echo "Run ssh-keygen from windows to generate workstation ssh"
-echo "Add new ssh key to github"
+Read-Host -Prompt "Press Enter to continue"
 echo "Update Windows Terminal to make WSL primary shell"
-echo "Pin \\wsl$\Ubuntu-20.04\home\steve for Quick Access in File Exploer (and put as shortcut on desktop)"
+Read-Host -Prompt "Press Enter to continue"
+echo "Pin \\wsl$\Ubuntu-20.04\home\steve for Quick Access in File Explorer (and put as shortcut on desktop)"
+Read-Host -Prompt "Press Enter to continue"
 echo "Start "X-Lanuch", go through wizard, save config...and shortcut to config to launch X-server"
-echo ""
-echo "In WSL"
-echo "  Copy .ssh from Windows Home to Linux Home"
-echo "  Run `code` from WSL to download Linux x64 vscode extensions"
+Read-Host -Prompt "Press Enter to continue"
+echo "In WSL: Run `code` from WSL to download Linux x64 vscode extensions"
+Read-Host -Prompt "Press Enter to continue"
